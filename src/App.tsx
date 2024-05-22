4c265810eed94bec0906014f24b8360b54c23d77
Eduardo Riki <eduardomatushita1@gmail.com>
2024-05-22 11:27:58 -0300

    feat: Configurar React Router DOM

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,11 @@
+import { BrowserRouter } from "react-router-dom";
+import { AppRoutes } from "./routes";
+
 export const App = () => {
-  return <div className="App">Teste</div>;
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 };
 
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,1 +1,10 @@
-export {};
+import { Navigate, Route, Routes } from "react-router-dom";
+
+export const AppRoutes = () => {
+  return (
+    <Routes>
+      <Route path="/pagina-inicial" element={<p>Página Inicial</p>} />
+      <Route path="*" element={<Navigate to="/pagina-inicial" />} />
+    </Routes>
+  );
+};
