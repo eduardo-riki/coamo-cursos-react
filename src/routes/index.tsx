0671c1e8bc0f93738fad9403c1b6d0c3c016d6b5
Eduardo Riki <eduardomatushita1@gmail.com>
2024-05-24 11:23:52 -0300

    feat: Adicionando menu lateral utilizando Drawer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,13 @@
 import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
-import { useAppThemeContext } from "../shared/contexts/ThemeContext";
+import { useAppDrawerContext } from "../shared/contexts";
 
 export const AppRoutes = () => {
-  const { toggleTheme } = useAppThemeContext();
+  const { toggleDrawerOpen } = useAppDrawerContext();
   return (
     <Routes>
       <Route path="/pagina-inicial" element={
-        <Button variant="contained" color="primary" onClick={toggleTheme}>Teste</Button>
+        <Button variant="contained" color="primary" onClick={toggleDrawerOpen}>Teste</Button>
       } />
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
---
+++ src/shared/contexts/DrawerContext.tsx
@@ -0,0 +1,32 @@
+import { createContext, useCallback, useContext, useState } from "react";
+
+interface IDrawerContextData {
+  isDrawerOpen: boolean;
+  toggleDrawerOpen: () => void;
+}
+
+const DrawerContext = createContext({} as IDrawerContextData);
+
+export const useAppDrawerContext = () => {
+  return useContext(DrawerContext);
+};
+
+interface IDrawerProviderProps {
+  children: React.ReactNode;
+}
+
+export const AppDrawerProvider: React.FC<IDrawerProviderProps> = ({
+  children,
+}) => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  const toggleDrawerOpen = useCallback(() => {
+    setIsDrawerOpen((oldDrawerOpen) => !oldDrawerOpen);
+  }, []);
+
+  return (
+    <DrawerContext.Provider value={{ isDrawerOpen, toggleDrawerOpen }}>
+      {children}
+    </DrawerContext.Provider>
+  );
+};
