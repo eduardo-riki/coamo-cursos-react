1fcf7eec56a16b189111813f0a9db6b044acb535
Eduardo Riki <eduardomatushita1@gmail.com>
2024-05-28 10:14:21 -0300

    feat: Inserir opções do menu lateral

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,14 +1,31 @@
 import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { useAppDrawerContext } from "../shared/contexts";
+import { useEffect } from "react";
 
 export const AppRoutes = () => {
-  const { toggleDrawerOpen } = useAppDrawerContext();
+  const { toggleDrawerOpen, setDrawerOptions } = useAppDrawerContext();
+
+  useEffect(() => {
+    setDrawerOptions([
+      { icon: "home", path: "/pagina-inicial", label: "Página Inicial" },
+    ]);
+  });
+
   return (
     <Routes>
-      <Route path="/pagina-inicial" element={
-        <Button variant="contained" color="primary" onClick={toggleDrawerOpen}>Teste</Button>
-      } />
+      <Route
+        path="/pagina-inicial"
+        element={
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={toggleDrawerOpen}
+          >
+            Teste
+          </Button>
+        }
+      />
       <Route path="*" element={<Navigate to="/pagina-inicial" />} />
     </Routes>
   );
