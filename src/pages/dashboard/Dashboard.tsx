52b0114043fa60cea366e245ff3c9694023fa27f
Eduardo Riki <eduardomatushita1@gmail.com>
2024-05-29 08:20:31 -0300

    feat: Adicionar barra de ferramenta

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -2,7 +2,7 @@ import { LayoutBaseDePagina } from "../../shared/layouts"
 
 export const Dashboard = () => {
   return (
-    <LayoutBaseDePagina titulo="Página Inicial">
+    <LayoutBaseDePagina titulo="Página Inicial" barraDeFerramentas="Barra de ferramentas">
       Teste
     </LayoutBaseDePagina>
   )
