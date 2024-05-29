9a01849b17589f50dc84318e7fe63b641568f1fd
Eduardo Riki <eduardomatushita1@gmail.com>
2024-05-29 08:20:53 -0300

    feat: Configurar responsividade do layout da página

--- src/shared/layouts/LayoutBaseDePagina.tsx
+++ src/shared/layouts/LayoutBaseDePagina.tsx
@@ -7,25 +7,29 @@ import {
   useMediaQuery,
 } from "@mui/material";
 import { useAppDrawerContext } from "../contexts";
+import { ReactNode } from "react";
 
 interface ILayoutBaseDePaginaProps {
   children: React.ReactNode;
   titulo: string;
+  barraDeFerramentas?: ReactNode;
 }
 
 export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({
   children,
   titulo,
+  barraDeFerramentas,
 }) => {
   const theme = useTheme();
-  const smDown = useMediaQuery(theme.breakpoints.up("sm"));
+  const smDown = useMediaQuery(theme.breakpoints.down("sm"));
+  const mdDown = useMediaQuery(theme.breakpoints.down("md"));
   const { toggleDrawerOpen } = useAppDrawerContext();
 
   return (
     <Box height="100%" display="flex" flexDirection="column" gap={1}>
       <Box
         padding={1}
-        height={theme.spacing(12)}
+        height={theme.spacing(smDown ? 6 : mdDown ? 8 : 12)}
         display="flex"
         alignItems="center"
       >
@@ -34,10 +38,19 @@ export const LayoutBaseDePagina: React.FC<ILayoutBaseDePaginaProps> = ({
             <Icon>menu</Icon>
           </IconButton>
         )}
-        <Typography variant="h5">{titulo}</Typography>
+        <Typography
+          variant={smDown ? "h5" : mdDown ? "h4" : "h3"}
+          whiteSpace="nowrap"
+          overflow="hidden"
+          textOverflow="ellipsis"
+        >
+          {titulo}
+        </Typography>
       </Box>
-      <Box>Barra de ferramenta</Box>
-      <Box>{children}</Box>
+      <Box>{barraDeFerramentas}</Box>
+      <Box flex={1} overflow="auto">
+        {children}
+      </Box>
     </Box>
   );
 };
