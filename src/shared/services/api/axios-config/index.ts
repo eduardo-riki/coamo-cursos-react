4fbe4c188c639174d3304fa3bcadd2a7608c792e
Eduardo Riki <eduardomatushita1@gmail.com>
2024-05-31 10:38:17 -0300

    chore: Adicionar ResponseInterceptor

--- src/shared/services/api/axios-config/index.ts
+++ src/shared/services/api/axios-config/index.ts
@@ -5,4 +5,9 @@ const Api = axios.create({
   baseURL: process.env.URL_BASE,
 });
 
+Api.interceptors.response.use(
+  (response) => responseInterceptor(response),
+  (error) => errorInterceptor(error)
+);
+
 export { Api };
