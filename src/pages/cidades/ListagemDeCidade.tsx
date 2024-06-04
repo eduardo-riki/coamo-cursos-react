d6ecf18a8cea30df2cf27d55bda3ad5316d1c856
Eduardo Riki <eduardomatushita1@gmail.com>
2024-06-04 10:54:44 -0300

    feat: Adicionar paginação e botões de edição e remoção

--- src/pages/cidades/ListagemDeCidade.tsx
+++ src/pages/cidades/ListagemDeCidade.tsx
@@ -5,23 +5,29 @@ import {
   CidadesServices,
   IListagemCidade,
 } from "../../shared/services/api/cidades/CidadesService";
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { UseDebounce } from "../../shared/hooks/UseDebounce";
 import {
   Icon,
   IconButton,
+  LinearProgress,
+  Pagination,
   Paper,
   Table,
   TableBody,
   TableCell,
   TableContainer,
+  TableFooter,
   TableHead,
   TableRow,
 } from "@mui/material";
+import { Environment } from "../../shared/environments";
 
 export const ListagemDeCidade: React.FC = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+
   const { debounce } = UseDebounce(300, true);
+  const navigate = useNavigate();
 
   const [cidades, setCidades] = useState<IListagemCidade[]>([]);
   const [totalCount, setTotalCount] = useState(0);
@@ -35,6 +41,25 @@ export const ListagemDeCidade: React.FC = () => {
     return Number(searchParams.get("pagina") || "1");
   }, [searchParams]);
 
+  const handleEdit = (id: number) => {
+    navigate("/cidades/editar/" + id.toString());
+  };
+
+  const handleRemove = (id: number) => {
+    if (window.confirm("Deseja realmente remover este registro?")) {
+      CidadesServices.deleteById(id).then((result) => {
+        if (result instanceof Error) {
+          alert(result.message);
+        } else {
+          alert("Registro apagado com sucesso!");
+          setCidades((oldCidade) => [
+            ...oldCidade.filter((oldCidade) => oldCidade.id !== id),
+          ]);
+        }
+      });
+    }
+  };
+
   useEffect(() => {
     setIsLoading(true);
 
@@ -45,7 +70,7 @@ export const ListagemDeCidade: React.FC = () => {
           alert(result.message);
         } else {
           setCidades(result.data);
-          setTotalCount(result.totalCount);
+          setTotalCount(Number(result.totalCount));
         }
       });
     });
@@ -58,8 +83,9 @@ export const ListagemDeCidade: React.FC = () => {
         <FerramentasDaListagem
           mostrarCampoDeBusca
           textoDaBusca={pesquisa}
+          aoClicarEmNovo={() => { navigate("/cidades/novo")}}
           aoMudarTextoDaBusca={(texto) =>
-            setSearchParams({ pesquisa: texto }, { replace: true })
+            setSearchParams({ pesquisa: texto, pagina: "1" }, { replace: true })
           }
         />
       }
@@ -82,16 +108,18 @@ export const ListagemDeCidade: React.FC = () => {
               <TableRow key={cidade.id}>
                 <TableCell>
                   <IconButton
-                    // onClick={() => handleEdit(cidade.id)}
+                    size="small"
+                    onClick={() => handleEdit(cidade.id)}
                     aria-label="Editar"
                   >
-                    <Icon>edit</Icon>
+                    <Icon fontSize="small"> edit</Icon>
                   </IconButton>
                   <IconButton
-                    // onClick={() => handleRemove(cidade.id)}
+                    size="small"
+                    onClick={() => handleRemove(cidade.id)}
                     aria-label="Remover"
                   >
-                    <Icon>delete</Icon>
+                    <Icon fontSize="small">delete</Icon>
                   </IconButton>
                 </TableCell>
                 <TableCell>{cidade.nome}</TableCell>
@@ -99,6 +127,36 @@ export const ListagemDeCidade: React.FC = () => {
               </TableRow>
             ))}
           </TableBody>
+
+          <TableFooter>
+            {isLoading ? (
+              <TableRow>
+                <TableCell colSpan={3}>
+                  <LinearProgress variant="indeterminate" />
+                </TableCell>
+              </TableRow>
+            ) : totalCount === 0 ? (
+              <TableRow>
+                <TableCell colSpan={3}>Nenhum registro encontrado.</TableCell>
+              </TableRow>
+            ) : totalCount > 0 && totalCount > Environment.LIMITE_DE_LINHAS ? (
+              <TableRow>
+                <TableCell colSpan={3} content="center">
+                  <Pagination
+                    shape="rounded"
+                    page={pagina}
+                    count={Math.ceil(totalCount / Environment.LIMITE_DE_LINHAS)}
+                    onChange={(_, newPage) => {
+                      setSearchParams(
+                        { pesquisa, pagina: newPage.toString() },
+                        { replace: true }
+                      );
+                    }}
+                  />
+                </TableCell>
+              </TableRow>
+            ) : null}
+          </TableFooter>
         </Table>
       </TableContainer>
     </LayoutBaseDePagina>
