5d44436bc17e0d0fc5f56c2892b3c5b850364e5b
Eduardo Riki <eduardomatushita1@gmail.com>
2024-06-06 11:34:36 -0300

    feat: Adicionar TextField genérico

---
+++ src/shared/forms/VTextField.tsx
@@ -0,0 +1,23 @@
+import { TextField } from '@mui/material';
+import { UseFormRegister } from 'react-hook-form';
+
+export const VTextField = ({ label, name, type, register, error }: { 
+  label: string, 
+  name: string, 
+  type: string, 
+  register: UseFormRegister<any>,
+  error: string | undefined 
+}) => {
+  return (
+    <TextField
+      size="small"
+      fullWidth
+      required
+      error={!!error}
+      helperText={error}
+      type={type}
+      label={label}
+      {...register(name, { required: true })}
+    />
+  )
+}
