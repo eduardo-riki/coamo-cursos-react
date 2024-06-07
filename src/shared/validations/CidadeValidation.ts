d07b627ae3380f7caa2af1667c589b9561209dfe
Eduardo Riki <eduardomatushita1@gmail.com>
2024-06-07 11:25:30 -0300

    fix: Corrigir mensagem de validação do Yup

--- src/shared/validations/CidadeValidation.ts
+++ src/shared/validations/CidadeValidation.ts
@@ -5,10 +5,10 @@ export const CidadeValidationSchema = Yup.object().shape({
     .string()
     .required("Nome da cidade é obrigatório.")
     .min(3, "Nome deve ter no mínimo 3 caracteres.")
-    .max(50, "Nome deve ter no máximo 100 caracteres."),
+    .max(50, "Nome deve ter no máximo 50 caracteres."),
   estado: Yup
     .string()
     .required("Nome do Estado é obrigatório.")
     .min(3, "Estado deve ter no mínimo 3 caracteres.")
-    .max(50, "Estado deve ter no máximo 100 caracteres."),
+    .max(50, "Estado deve ter no máximo 50 caracteres."),
 });
