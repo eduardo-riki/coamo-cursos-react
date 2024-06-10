a4bef845465762ba0dbc23af796128e521701ff6
= <=>
2024-06-10 00:00:32 -0300

    fix: Corrigir validação Yup, retirando suas mensagens

--- src/shared/validations/CidadeValidation.ts
+++ src/shared/validations/CidadeValidation.ts
@@ -1,14 +1,6 @@
 import * as Yup from "yup";
 
 export const CidadeValidationSchema = Yup.object().shape({
-  nome: Yup
-    .string()
-    .required("Nome da cidade é obrigatório.")
-    .min(3, "Nome deve ter no mínimo 3 caracteres.")
-    .max(50, "Nome deve ter no máximo 50 caracteres."),
-  estado: Yup
-    .string()
-    .required("Nome do Estado é obrigatório.")
-    .min(3, "Estado deve ter no mínimo 3 caracteres.")
-    .max(50, "Estado deve ter no máximo 50 caracteres."),
+  nome: Yup.string().required().min(3).max(50),
+  estado: Yup.string().required().min(3).max(50),
 });
