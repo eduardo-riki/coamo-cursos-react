40a44becb409c3b27ce8464d6ce42b956a60c2a1
= <=>
2024-06-11 00:02:30 -0300

    fix: Separar validações de SignIn e SignUp

---
+++ src/shared/validations/SignUpValidation.ts
@@ -0,0 +1,14 @@
+import * as Yup from "yup";
+
+export const SignUpValidationSchema = Yup.object().shape({
+  nome: Yup.string().required().min(3).max(100),
+  email: Yup.string().email().required().min(3).max(100),
+  senha: Yup.string()
+    .required()
+    .min(6)
+    .max(100)
+    .test("senhas-iguais", "As senhas não coincidem", function (value) {
+      return value === this.parent.confirmarSenha;
+    }),
+  confirmarSenha: Yup.string().required().min(6).max(100),
+});
